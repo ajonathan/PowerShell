4d596c11f566128380f8d4591c316b3fbe92edf7
Jonathan <jonathan-andersson@outlook.com>
2019-05-21 15:32:18 +0200

    Add files via upload

---
+++ createVmDomainJoin.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script creates a VM and join it to a domain
+
+rg="serverrg"
+vmname="servername"
+vnet="vnet"
+subnet="subnet"
+vnetrg="vnetrg"
+location="westeurope"
+image="win2016datacenter"
+vmsize="Standard_DS2_v2"
+localadminuser="username"
+domainname="contoso.com"
+domainuser="username"
+read -p "Enter your local admin user password: " -s localadminpw
+echo
+read -p "Enter your domain joining user password: " -s domainjoinpw
+echo
+
+# Get Subnet id
+sub=$(az network vnet subnet show -g "$vnetrg" --vnet-name "$vnet" --name "$subnet" -o tsv --query id)
+
+echo "Creating virtual network interface"
+az network nic create --resource-group "$rg" --name $vmname-nic --subnet "$sub" --public-ip-address ""
+
+echo "Creating VM"
+az vm create --resource-group $rg --name $vmname --nics "$vmname-nic" --location $location --license-type None --image $image --size $vmsize --admin-username $localadminuser --admin-password $localadminpw
+
+echo "Adding VM to domain"
+az vm extension set -n JsonADDomainExtension --publisher Microsoft.Compute --version 1.3.2 --vm-name $vmname -g $rg --settings '{"Name" : "'$domainname'", "User" : "'$domainname\\$domainuser'", "Restart" : "true", "Options" : 3}' --protected-settings '{"Password": "'$domainjoinpw'"}'
