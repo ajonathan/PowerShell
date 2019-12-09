0cc66ec27ab809e291898fab070cd85ab739f94f
Jonathan Andersson <joa@microsoft.com>
2019-12-09 10:42:48 +0900

    Changed for getting tags

--- RemoveResourceGroupAutomation.ps1
+++ RemoveResourceGroupAutomation.ps1
@@ -1,10 +1,23 @@
 <#
     .DESCRIPTION
         Removes Resource Groups that have a tag "RemoveResourceGroup" sat to "Yes"
+        Script can be used in both Azure Automation and direct from PowerShell prompt
 
     .NOTES
         Author: Jonathan Andersson
-        Last Updated: 12/06/2019
+        Last Updated: 12/09/2019
+
+    .PARAMETER TagResourceGroupName
+        Tag name
+
+    .PARAMETER TagValue
+        Tag value
+
+    .PARAMETER AzureAutomation
+        If script sould be run in Azure Automation
+
+    .PARAMETER ConnectionName
+        A<ure Automation RunAs Connection to Azure
 
     .EXAMPLE
         RemoveResourceGroupAutomation -TagResourceGroupName "RGName" -TagValue "Yes"
@@ -18,25 +31,50 @@ param (
     
     [Parameter()]
     [string]
-    $TagValue = "Yes"
+    $TagValue = "Yes",
+
+    [Parameter()]
+    [bool]
+    $AzureAutomation = $true,
+
+    [Parameter()]
+    [string]
+    $ConnectionName = "AzureRunAsConnection"
 )
-Process {
-    try {
-        [object] $Tag = @{}
-        $Tag.Add($TagResourceGroupName, $TagValue)
 
-        Write-Output "Using TagResourceGroupName: $TagResourceGroupName and TagValue: $TagValue"
+# Create a Tag object
+[object] $Tag = @{}
 
-        $ResourceGroups = Get-AzResourceGroup -tag $tag
-        
-        foreach ($ResourceGroup in $ResourceGroups) {
-            Remove-AzResourceGroup -Name $ResourceGroup.ResourceGroupName -Force
-            Write-Output "Removed Resource Group: $ResourceGroup.ResourceGroupName"
-        }
+try {
+    if ($AzureAutomation) {
+        # Get the connection "AzureRunAsConnection "
+        $servicePrincipalConnection = Get-AutomationConnection -Name $ConnectionName         
+
+        # Logging in to Azure
+        Add-AzureRmAccount `
+            -ServicePrincipal `
+            -TenantId $servicePrincipalConnection.TenantId `
+            -ApplicationId $servicePrincipalConnection.ApplicationId `
+            -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
     }
-    catch {
-        Write-Output "Trouble"
-        Write-Error -Message $_.Exception
-        throw $_.Exception
+    $Tag.Add($TagResourceGroupName, $TagValue)
+    Write-Output "Using TagResourceGroupName: $TagResourceGroupName and TagValue: $TagValue"
+
+    $ResourceGroups = Get-AzureRmResourceGroup -Tag $Tag
+
+    foreach ($ResourceGroup in $ResourceGroups) {
+        Remove-AzureRmResourceGroup -Name $ResourceGroup.ResourceGroupName -Force
+        Write-Output "Removed Resource Group: $ResourceGroup.ResourceGroupName"
     }
+} 
+catch {
+	if (!$servicePrincipalConnection)
+	{
+		$ErrorMessage = "Connection $ConnectionName not found."
+		throw $ErrorMessage
+    } 
+    else{
+		Write-Error -Message $_.Exception
+		throw $_.Exception
+	}
 }
