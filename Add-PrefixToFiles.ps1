81b16af717df7f708ad3ae45d6ba9f9f3c614889
Jonathan <jonathan-andersson@outlook.com>
2020-06-22 16:13:25 +0900

    Removed last line

--- Add-PrefixToFiles.ps1
+++ Add-PrefixToFiles.ps1
@@ -1,61 +1,84 @@
-function PSTemplate {
+<#
+    .DESCRIPTION    
+        Will add a prefix on files and folders
 
-    <#
-        https://github.com/PoshCode/PowerShellPracticeAndStyle/blob/master/Style-Guide/Documentation-and-Comments.md#doc-01-write-comment-based-help
-        .DESCRIPTION    
-            Description
+    .NOTES
+        CREATED WITH: Visual Studio Code
+        AUTHOR: Jonathan
+        LAST UPDATED: 06/21/2020
+        FILENAME: Add-PrefixToFiles.ps1
+
+    .PARAMETER Prefix
+        Prefix that should be added to the file or folder
+
+    .PARAMETER AlwaysPrefix
+        Should the script always add the prefix or only on files that doesn't have the prefix already
+
+    .PARAMETER File
+        Only add prefix to files
+
+    .PARAMETER Folder
+        Only add prefix to folders
+
+    .SYNOPSIS
+        Synopsis
+
+    .EXAMPLE
+        Add-PrefixToFiles.ps1 -Prefix "My"
+        Add-PrefixToFiles.ps1 -Prefix "g-" -AlwaysPrefix $false -Directory $true
+#>
     
-        .NOTES
-            CREATED WITH: Visual Studio Code
-            AUTHOR: Jonathan
-            FILENAME: PSTemplate.ps1
-    
-        .PARAMETER parameter1
-            First parameter
-    
-        .SYNOPSIS
-            Synopsis
-    
-        .EXAMPLE
-            Example
-    #>
-    
-        [CmdletBinding (DefaultParametersetName="first")]
-        param (
-            # First parameter
-            [Parameter (ParameterSetName="first", Mandatory=$true)]
-            [string]
-            $FirstParameter,
-    
-            # Second parameter
-            [Parameter (ParameterSetName="second", Mandatory=$true)]
-            [ValidateSet("one", "two")]
-            [string]
-            $SecondParameter
-        )
-        begin {
-            $Time = Get-Date
-        }
-        process {
-            try {
-                if (-not (Get-Item -Path c:\Logs)) {
-                    New-Item -Path c:\Logs -ItemType Directory
+[CmdletBinding (DefaultParametersetName="first")]
+param (
+    # First parameter
+    [Parameter (ParameterSetName="first", Mandatory=$true)]
+    [string]
+    $Prefix,
+
+    # Second parameter
+    [Parameter (Mandatory=$false)]
+    [bool]
+    $AlwaysPrefix,
+
+    # Second parameter
+    [Parameter (Mandatory=$false)]
+    [bool]
+    $File,
+
+    # Second parameter
+    [Parameter (Mandatory=$false)]
+    [bool]
+    $Directory
+)
+begin {
+    if ($Directory -eq $true) {
+        $items = Get-ChildItem -Directory *
+    } elseif ($File -eq $true) {
+        $items = Get-ChildItem -File *
+    } else {
+        $items = Get-ChildItem *
+    }
+}
+process {
+    try {
+        foreach ($item in $items) {
+            $fileName = $item.Name
+            if ($fileName -ne "Add-PrefixToFiles.ps1") {
+                if ($fileName.Substring(0, $Prefix.Length) -eq "$Prefix" -and $AlwaysPrefix -eq $false) {
+                    Write-Host "Prefix already exist - " $Prefix ": " $fileName
                 }
-                
-                Write-Host "$FirstParameter $SecondParameter"
-                $LastBootUpTime = (Get-CimInstance -ClassName win32_operatingsystem).LastBootUpTime
-                Write-Host "LastBootUpTime: $LastBootUpTime"
-                "$Time - LastBootUpTime $LastBootUpTime" | out-file c:\Logs\PSTemplate.log -append
+                    else {
+                    Rename-Item $fileName "$prefix$fileName"
+                    Write-Host $prefix$fileName
+                }
             }
-            catch {
-                $ErrorMessage = $_.Exception.Message
-                #$FailedItem = $_.Exception.ItemName
-                "$Time - $ErrorMessage" | out-file c:\Logs\PSTemplate.log -append
-            }
-            
         }
-        end {}
+    }
+    catch {
+        $ErrorMessage = $_.Exception.Message
+        #$FailedItem = $_.Exception.ItemName
+        "$Time - $ErrorMessage" | out-file c:\Logs\AddPrefixToFiles.log -append
     }
     
-    PSTemplate -FirstParameter "parameter1"
-    
+}
+end {}
